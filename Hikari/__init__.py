c06c4d12dd08439792f841aa440118b5bf0fb733
ingex music <137978140+Masexrobot@users.noreply.github.com>
2023-07-14 21:22:23 +0800

    update __init__.py

--- Hikari/__init__.py
+++ Hikari/__init__.py
@@ -40,7 +40,7 @@ logging.getLogger('ptbcontrib.postgres_persistence.postgrespersistence').setLeve
 LOGGER = logging.getLogger('[Hikari]')
 LOGGER.info("Joox Music is starting. | An Joox Music Parts. | Licensed under GPLv3.")
 LOGGER.info("Not affiliated to other anime or Villain in any way whatsoever.")
-LOGGER.info("Project maintained by: github.com/Robotikaazazel (t.me/cdkagenouu)")
+LOGGER.info("Project maintained by: github.com/Robotikaazazel (t.me/masex77)")
 
 # if version < 3.9, stop bot.
 if sys.version_info[0] < 3 or sys.version_info[1] < 8:
